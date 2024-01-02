73986988e95903660aa66ad1b6a5d7dc3651b09a
Kha Pham <49766423+Mono98ui@users.noreply.github.com>
2024-01-01 22:46:13 -0500

    edit page complete

--- react-crud/src/pages/EditPage.jsx
+++ react-crud/src/pages/EditPage.jsx
@@ -1,7 +1,87 @@
+import axios from "axios"
+import {useState, useEffect} from "react"
+import {useParams} from "react-router-dom"
+import {useNavigate} from "react-router-dom"
+import {toast} from 'react-toastify';
+
 const EditPage = () => {
+	let {id} = useParams()
+	const [isLoading, setIsloading] = useState(false)
+	const [product, setProduct] = useState({
+		name:"",
+		quantity:"",
+		price:"",
+		image:"",
+	})
+	const navigate = useNavigate()
+
+	const getProduct = async() =>{
+		setIsloading(true)
+
+		try{
+			const response = await axios.get(`https://node-api-xts8.onrender.com/api/products/${id}`)
+		setProduct({
+			name: response.data.name,
+			quantity:response.data.quantity,
+			price:response.data.price,
+			image:response.data.image,
+		})
+		setIsloading(false)
+		}catch(error){
+			setIsloading(false)
+			toast.error(error.message)
+
+		}
+
+	}
+
+	const updateProduct = async(e) => {
+		e.preventDefault()
+		if(product.name === "" || product.quantity === "" || product.price === "" || product.image === ""){
+			alert('Please fill out all input completely')
+			return
+		}
+
+		try{
+			setIsloading(true)
+			const response = await axios.put(`https://node-api-xts8.onrender.com/api/products/${id}`,product)
+			toast.success(`Updated ${response.data.name} sucessfuly`)
+			setIsloading(false)
+			navigate("/")
+		}
+		catch(error){
+			console.log(error)
+			toast.error(error.message)
+			setIsloading(false)
+		}
+	}
+
+	useEffect(()=>{
+		getProduct()
+	},[])
 	return(
-		<div>
-			This Edit Page
+		<div className="max-w-lg bg-white shadow-lg mx-auto p-7 rounded mt-6">
+			<h2 className="font-semibold text-2xl mb-4 block text-center">
+				Update a Product - {product.name}
+			</h2>
+			<form onSubmit={updateProduct}>
+				<div className="space-y-2">
+					<div>
+						<label>Name</label>
+						<input type="text" value={product.name} onChange={(e) =>setProduct({...product,name: e.target.value})} className="w-full block border p-3 text-gray-600 rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder-gray-400" placeholder="Enter Name"/>
+						<label>Quatity</label>
+						<input type="number" value={product.quantity} onChange={(e) =>setProduct({...product,quantity: e.target.value})} className="w-full block border p-3 text-gray-600 rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder-gray-400" placeholder="Enter Quantity"/>
+						<label>Price</label>
+						<input type="number" value={product.price} onChange={(e) =>setProduct({...product,price: e.target.value})} className="w-full block border p-3 text-gray-600 rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder-gray-400" placeholder="Enter Price"/>
+						<label>Image URL</label>
+						<input type="text"  value={product.image} onChange={(e) =>setProduct({...product,image: e.target.value})} className="w-full block border p-3 text-gray-600 rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder-gray-400" placeholder="Enter Image URL"/>
+						<div>
+							{!isLoading && (<button className="block w-full mt-6 bg-blue-700 text-white rounded-sm px-4 py-2 font-bold hover:bg-blue-600 hover:cursor-pointer">Update</button>)}
+						</div>
+					</div>
+				</div>
+
+			</form>
 		</div>
 	)
 }
